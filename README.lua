5148e9a0adf958d29c5ccca411d932917c04d973
funnykayan <funnykayan54321@gmail.com>
2024-12-19 08:35:13 +0100

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,1 +1,19 @@
+--[[ Funnykayan's API Unciphering script V1.0.0 LUA
+-- Github link: https://github.com/funnykayan/API-Project/tree/main
+
+-- Settings available: 
+APIKEY: Put your API key here, used for unciphering.
+APITOKEN: Put your API token here, only use this for very specific use cases, not required.
+IF YOU ARE NOT USING APITOKEN, INPUT "TOKENNOTINUSE" SO THE SCRIPT DOESNT STOP IT!
+APILinkOne: Primary link template, you can customize if you have your own custom link.
+APILinkTwo: Secondary link, not required. Input template here if you have your own custom link.
+APILinkThree: Tertiary link, not required. Input template here if you have your own custom link.
+Enabled: Setting to enable or disable the script.
+Tutorial: Only works if all other settings are disabled, except debug. Shows the steps the script follows in the output.
+ExtraResults: Shows more in depth results, !!ExtraResults cannot work while Simplified is activated!!
+Simplified: Shows short results, !!Simplified cannot work while ExtraResults is activated!!
+UserDebugMode: Debugging, enable other settings to debug settings.
+
+I don't recommend editting/modifying this script if you dont have much experience with lua.
+However, im not stopping you from doing so. --]]
 
